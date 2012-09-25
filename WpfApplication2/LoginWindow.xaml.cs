c735521f5f67ab0cf9e8f21bbd701a997a920b8b
rsrahulee <rahul@weboniselab.com>
2012-09-25 18:35:51 +0530

    new login screen added

---
+++ WpfApplication2/LoginWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mMainWindow = new MainWindow();
+            mMainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WpfApplication2/StartupWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for StartupWindow.xaml
+    /// </summary>
+    public partial class StartupWindow : Window
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            //LoginWindow login = new LoginWindow();
+            //login.Show();
+            //NavigationController.NavigateTo(new LoginWindow());
+        }
+
+        private void MenuItemNew_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow login = new LoginWindow();
+            login.Show();
+        }
+    }
+}
